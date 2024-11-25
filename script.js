84bf6a68f7eb97f634eb6f6b9dd46bd446cc0d2b
B10-N <nsibibadis@gmail.com>
2024-11-25 11:39:49 +0100

    DOM

---
+++ script.js
@@ -0,0 +1,17 @@
+let ca1 =document.getElementById('ca1')
+let ca2 =document.getElementById('ca2')
+let ca3 =document.getElementById('ca3')
+
+
+
+
+
+ca1.addEventListener('click' ,(e) =>{
+    document.body.style.backgroundColor= "blue"
+})
+ca2.addEventListener('click' ,(e) =>{
+    document.body.style.backgroundColor= "black"
+})
+ca3.addEventListener('click' ,(e) =>{
+    document.body.style.backgroundColor= "green"
+})
